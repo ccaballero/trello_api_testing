771063523fe819b3c86aae422e8bd54b43c031a0
Ubaldo Villaseca <ubaldo.villaseca@jalasoft.com>
2019-02-22 21:06:00 -0400

    .

--- features/lists.feature
+++ features/lists.feature
@@ -5,6 +5,20 @@ Feature: CRUD lists
   Scenario: Create list
     Given I have an existing board
     When I send a POST request to /lists with name myName
-    Then I get a response with  HTTP status code 200
-      And with valid body
+    Then I get a resp0nse with  HTTP status code 200
+      And with valid b0dy
+        """
+        {
+            "type":"object",
+            "properties":{
+                "id":{type:"string"},
+                "name":{type:"string"},
+                "closed":{type:"boolean"},
+                "idBoard":{type:"string"},
+                "pos":{type:"integer"},
+                "limits":{type:"array"}
+            },
+            "required":["id","name","closed","idBoard", "pos", "limits"]
+        }
+        """
     
--- features/steps/lists.py
+++ features/steps/lists.py
@@ -3,28 +3,27 @@ import requests
 import uuid
 from compare import expect
 import json
+from jsonschema import validate
 
 @given('I have an existing board')
 def step_impl(context):
   board_name = uuid.uuid4()
-  print(context.url)
+  print(f'{context.url}/boards/?name={board_name}&key={context.key}&token={context.token}')
   r = requests.post(f'{context.url}/boards/?name={board_name}&key={context.key}&token={context.token}')
   expect(r.status_code).to_equal(requests.codes.ok)
-  context.board_id = json.load(r.text)
+  context.board_id = r.json()["id"]
   
 @when(u'I send a POST request to {endpoint} with name {name}')
 def step_impl(context, endpoint, name):
   r = requests.post(f'{context.url}{endpoint}?name={name}&idBoard={context.board_id}&key={context.key}&token={context.token}')
-  context.response = r.text
+  context.response = r
 
 
-@then(u'I get a response with  HTTP status code {status_code}')
+@then(u'I get a resp0nse with  HTTP status code {status_code:d}')
 def step_impl(context, status_code):
-  expect(context.response.status_code).to_equals(status_code)
+  expect(context.response.status_code).to_equal(status_code)
   
 
-#   @then('with valid body')
-#   def step_impl(context, status_code):
-#       pass
-#
-  
+@then('with valid b0dy')
+def step_impl(context):
+  validate(context.response, context.text)
