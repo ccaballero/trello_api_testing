19ca1a34cc639c45146b4aa4f999fdb6025cde5f
mika36 <46766024+mika36@users.noreply.github.com>
2019-02-25 18:40:44 -0400

    Card Creation

--- features/cards.read.feature
+++ features/cards.read.feature
@@ -5,14 +5,13 @@
 Feature: Cards management GET
   @acceptance @read
   Scenario: Read information of a pre-established card
-    Given I have an existing card with parameters
-            |   PARAMETER | VALUE               |
-            |        name | {organization}      |
-            | displayName | example             |
-            |        desc | description example |
-            |     website | http://example.io   |
+    Given I have an already created card with parameters
+            | QUERY PARAMETER | VALUE               |
+            |            name | {card}              |
+            |            desc | description example |
+            |          idList | {list_id}           |
     When I send a GET request to /cards/{id}
     Then I get a response status code 200
-    And I get a return values
+
 
 
--- features/steps/cards.py
+++ features/steps/cards.py
@@ -65,3 +65,33 @@ def step_impl(context):
 
     context.generated['{list_id}'] = body['id']
 
+@given(u'I have an already created card with parameters')
+def step_impl(context):
+    query = {
+        'key':context.key,
+        'token':context.token
+    }
+
+    for row in context.table:
+        if match('\{[A-Za-z0-9_\-]+\}',row[1]):
+            if 'generated' not in context:
+                context.generated = {}
+
+            context.generated[row[1]] = 'o'+str(uuid4()).replace('-','')
+            query[row[0]] = context.generated[row[1]]
+        else:
+            query[row[0]] = row[1]
+
+    response = request('POST',context.url+'/cards',params=query)
+    print('==> query parameters:',query)
+
+    expect(response.status_code).to_equal(200)
+
+    body = loads(response.text)
+    context.card_id = body['id']
+
+    if 'generated' not in context:
+        context.generated = {}
+
+    context.generated['{card_id}'] = body['id']
+
